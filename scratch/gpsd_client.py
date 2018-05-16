2e5a2e93283fd1f54fc4385083845a6dad0f897c
nagarkar <chinmay.nagarkar@gmail.com>
2018-05-16 06:41:51 +0000

    fixing premature commit

--- scratch/gpsd_client.py
+++ scratch/gpsd_client.py
@@ -4,27 +4,40 @@
 import serial
 import sys
 import gps
+import curses
 
 host = sys.argv[1]
 port = sys.argv[2]
 
-session = gps.gps(host, port)
-session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
-while True:
+# Helper method to work around the fact python doesnt' support i++ syntax
+def _i():
+  _i.count += 1
+  return _i.count;
+
+# Main method
+def run(stdscr):
+  session = gps.gps(host, port)
+  session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
+
+  stdscr.clear()
+  done = False
+  while not done:
 	report = session.next()
+	
+	_i.count = 0
+	stdscr.addstr(_i(),0, "Type x to exit")
 	if (report['class'] == 'TPV'):
 		# Print the attributes. 
 		# For full list, see http://www.catb.org/gpsd/gpsd_json.html
-		sys.stdout.write("TIME: 	%s " %report.time)
-		sys.stdout.write("GPS Status: 	%d " %report.status)
-		sys.stdout.write("Latitude: 	%d " %report.lat)
-		sys.stdout.write("Longitude: 	%d " %report.lon)
-		sys.stdout.write("Speed: 	%d " %report.speed)
-		sys.stdout.write("Course: 	%d " %report.track)
-		
-
-		# We want to print in place on console
-		# See https://goo.gl/bhDYz2 for explanation of this trick.
-		sys.stdout.write("\r")
-		sys.stdout.flush()
-
+		stdscr.addstr(_i(),0, "Time: 	%s" %report.time)
+		stdscr.addstr(_i(),0, "Device:%s" %report.device)
+		stdscr.addstr(_i(),0, "Lat: 	%s" %report.lat)
+		stdscr.addstr(_i(),0, "Long: 	%s" %report.lon)
+		stdscr.addstr(_i(),0, "Speed: 	%s" %report.speed)
+		stdscr.addstr(_i(),0, "Track: 	%s" %report.track)
+		stdscr.refresh();
+	stdscr.nodelay(True)
+	key = stdscr.getch()
+	if key == ord('x'):
+		done = True
+curses.wrapper(run)
