2679bf3c255f5f1b97c920eb1aabe599dbc7c7a7
nagarkar <chinmay.nagarkar@gmail.com>
2018-05-27 02:22:38 +0000

    Modified prompt logic

--- utilities/linux/.smart.bash.prompt.py
+++ utilities/linux/.smart.bash.prompt.py
@@ -5,6 +5,7 @@ username = os.environ['USER']
 pwd = os.getcwd()
 homedir = os.path.expanduser('~')
 pwd = pwd.replace(homedir, '~', 1)
-if len(pwd) > 33:
-    pwd = pwd[:10]+'...'+pwd[-20:] # first 10 chars+last 20 chars
-print '[%s@%s:%s] ' % (username, hostname, pwd)
+if len(pwd) > 28:
+    pwd = pwd[:5]+'...'+pwd[-20:] # first 10 chars+last 20 chars
+#print '[%s@%s:%s] ' % (username, hostname, pwd)
+print '[%s:%s] ' % (username, pwd)
